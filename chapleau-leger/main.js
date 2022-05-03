bd24937b37a6148cee895f208d5dbc1809a07aba
Trish Nguyen <70410218+Trishng@users.noreply.github.com>
2022-05-03 15:32:09 -0400

    regler swiper

--- chapleau-leger/main.js
+++ chapleau-leger/main.js
@@ -1,18 +1,20 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    slidesPerView: 'auto',
-    slidesPerGroup: 1,
-    spaceBetween: 40,
-    loop: true,
-    speed: 1000,
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-      draggable: true,
-    },
+setTimeout(() => {
+  const swiper = new Swiper('.swiper', {
+      // Optional parameters
+      slidesPerView: "auto",
+      spaceBetween: 40,
+      speed: 1000,
+      loop: true,
+    
+      // And if we need scrollbar
+      scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+      },
 
-  });
+    });
+},1000);
+//quand tous les images se charge, executer, a changer
 
 let menu = document.getElementById("menu");
 let icon = document.querySelector('.icon');
